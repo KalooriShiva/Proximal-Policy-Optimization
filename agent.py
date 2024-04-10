588c84e2d119c90daf00b33ebc1fa5180021cacf
Shivaksp <84005126+Shivaksp@users.noreply.github.com>
2024-04-10 13:06:29 +0530

    Changed agent according to the Monte Carlo

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
+import tensorflow_probability as tfp
 
 
 class ReplayMemory_MPC:
@@ -46,9 +47,12 @@ class DQNAgent:
         self.epsilon = epsilon
         self.batch_size = batch_size
         self.memory = ReplayMemory(replay_memory_size, 2*self.env.state_dim + self.env.action_dim + 1)
-        self.Q = self._get_model(nn_arch)
-        self.Q_t = self._get_model(nn_arch)
+        self.A = self.actor_model(nn_arch)
+        self.Q = self.critic_model(nn_arch)
+        self.Q_t = self.critic_model(nn_arch)
         self.Q_t.set_weights(self.Q.get_weights())
+        self.A_opt = tf.keras.optimizers.Adam(learning_rate=1e-3)
+        self.Q_opt = tf.keras.optimizers.Adam(learning_rate=1e-3)
         self.optimizer = tf.keras.optimizers.legacy.RMSprop(learning_rate=learning_rate, decay=decay_rate)
         self.reset_steps = reset_steps
         self.state_index = np.arange(0, self.env.state_dim, 1, dtype=np.int16)
@@ -56,60 +60,81 @@ class DQNAgent:
         self.next_state_index = np.arange(self.action_index[-1]+1, self.action_index+1+self.env.state_dim, 1, dtype=np.int16)
         self.reward_index = self.next_state_index[-1] + 1
         self.memory_mpc = ReplayMemory_MPC(200)
+        self.returns = []
         
-    def _get_model(self, nn_arch):
+    def critic_model(self, nn_arch):
         model = tf.keras.models.Sequential()
         model.add(tf.keras.layers.Dense(nn_arch[0], input_shape=(4,), activation="relu"))
         for num_neurons in nn_arch[1:]:
             model.add(tf.keras.layers.Dense(num_neurons, activation="relu"))
-        model.add(tf.keras.layers.Dense(self.env.num_j_temp, activation="linear"))
+        model.add(tf.keras.layers.Dense(1, activation="linear"))
+        model.build()
+        return model
+    
+    def actor_model(self, nn_arch):
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(nn_arch[0], input_shape=(4,), activation="relu"))
+        for num_neurons in nn_arch[1:]:
+            model.add(tf.keras.layers.Dense(num_neurons, activation="relu"))
+        model.add(tf.keras.layers.Dense(self.env.num_j_temp, activation="softmax"))
         model.build()
         return model
     
     @tf.function
-    def greedy_policy(self, state):
-        return tf.argmax(self.Q(tf.reshape(state, (1, -1)))[0])
+    def policy(self,state):
+        prob = self.A(np.array([state]))
+        prob = prob.numpy()
+        dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
+        action = dist.sample()
+        return int(action.numpy()[0])
     
-    def policy(self, state):
-        if np.random.uniform() > self.epsilon: return self.greedy_policy(state)
-        return np.random.randint(self.env.num_j_temp)
+    def learn(self, states, actions,  adv , old_probs, discnt_rewards):
+        discnt_rewards = tf.reshape(discnt_rewards, (len(discnt_rewards),))
+        adv = tf.reshape(adv, (len(adv),))
+
+        old_p = old_probs
+
+        old_p = tf.reshape(old_p, (len(old_p),2))
+        with tf.GradientTape() as tape1, tf.GradientTape() as tape2:
+            p = self.actor(states, training=True)
+            v =  self.critic(states,training=True)
+            v = tf.reshape(v, (len(v),))
+            td = tf.math.subtract(discnt_rewards, v)
+            c_loss = 0.5 * kls.mean_squared_error(discnt_rewards, v)
+            a_loss = self.actor_loss(p, actions, adv, old_probs, c_loss)
+            
+        grads1 = tape1.gradient(a_loss, self.A.trainable_variables)
+        grads2 = tape2.gradient(c_loss, self.Q.trainable_variables)
+        self.A_opt.apply_gradients(zip(grads1, self.A.trainable_variables))
+        self.Q_opt.apply_gradients(zip(grads2, self.Q.trainable_variables))
+        return a_loss, c_loss
     
-    @tf.function
-    def opt_q_val_many_states(self, states):
-        """ Calculates action based on Q matrix.
-        Parameters: 
-            model: the neural network that takes state as input and gives Q values for different actions as outputs
-            states: a n x 3 numpy array"""
-        
-        q_action_arrays = self.Q_t(states)
-        opt_q_values = tf.reduce_max(q_action_arrays, axis=1)
-        terminal_states_indexes = tf.where(states[:, 0] == self.env.n_tf)
-        # # opt_q_values[terminal_states_indexes] = 0
-        # terminal_states_indexes = terminal_states_indexes[:, 0]
-        # print(terminal_states_indexes)
-        update = tf.zeros(shape=(tf.shape(terminal_states_indexes)[0],))
-        opt_q_values = tf.tensor_scatter_nd_update(opt_q_values, 
-                                                   terminal_states_indexes, 
-                                                   update,
-                                                   )
-        return opt_q_values
+    def preprocess(states, actions, rewards, done,values, returns, gamma):
+        g = 0
+        lmbda = 0.95
+        for i in reversed(range(len(rewards))):
+            delta = rewards[i] + gamma * values[i + 1] * done[i] - values[i]
+            g = delta + gamma * lmbda * done[i] * g
+            returns.append(g + values[i])
+
+        returns.reverse()
+        adv = np.array(returns, dtype=np.float32) - values[:-1]
+        adv = (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
+        states = np.array(states, dtype=np.float32)
+        actions = np.array(actions, dtype=np.int32)
+        returns = np.array(returns, dtype=np.float32)
+        return states, actions, returns, adv 
+    
+    
+    def value_function(self,states):
+        values = []
+        for i in states:
+           values.append(self.Q(np.array(states[i])))
+        return values
     
     def get_action(self, state):
         action_index = self.greedy_policy(np.hstack([state.reshape(-1)]))
         return self.env.tj_list[action_index]
-    
-    @tf.function
-    def update_q_weights(self, inputs, action_args, labels):
-        
-        # training model
-        masks = tf.one_hot(action_args, self.env.num_j_temp)
-        masks = tf.reshape(masks, (masks.shape[0], masks.shape[2]))
-        with tf.GradientTape() as tape:
-            Q_values = self.Q(inputs)
-            Q_action = tf.reduce_sum(tf.multiply(Q_values, masks), axis=1)
-            loss = tf.keras.losses.mse(labels, Q_action)
-        gradients = tape.gradient(loss, self.Q.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.Q.trainable_variables))
     
     def train(self, num_episodes):
         iter_num = 0
@@ -124,39 +149,39 @@ class DQNAgent:
             # initialize markov chain with initial state
             state = self.env.reset()
             cumulative_reward = 0
+            state = self.env.reset()
+            all_aloss = []
+            all_closs = []
+            rewards = []
+            states = []
+            actions = []
+            probs = []
+            dones = []
+            values = []
             while not self.env.done:
-                # epsilon greedy action selection
                 action_index = self.policy(state)
+                value = self.critic_model(state)
                 action = self.env.tj_list[action_index]
-                #print("action",action)
-                # executing action, observing reward and next state to store experience in tuple
-                next_state, reward, done, info = self.env.step(action)
+                next_state, reward, done, _ = self.env.step(action)
+                dones.append(1-done)
+                rewards.append(reward)
+                states.append(state)
+                #actions.append(tf.one_hot(action, 2, dtype=tf.int32).numpy().tolist())
+                actions.append(action)
+                prob = self.A(np.array([state]))
+                probs.append(prob[0])
+                values.append(value[0][0])
                 cumulative_reward += (self.discount_factor ** iter_num) * reward
-                # store experience in replay memory
-                self.memory.push(np.hstack([state.reshape(-1), action_index, next_state.reshape(-1), reward]))
-                
-                # get replay memory
-                if self.memory.size < self.batch_size: 
-                    state = next_state
-                    continue
-                # get replay memory from MPC
-                rand_batch_mpc = self.memory_mpc.get_batch(size=int(self.batch_size/2))
-                rand_batch_replay = self.memory.get_batch(size=int(self.batch_size/2))
-                rand_batch = np.append(rand_batch_mpc,rand_batch_replay, axis=0)
-                # print("------------------------------------------------------------------------------------------")
-                # print(rand_batch)
-                inputs = rand_batch[:, self.state_index]
-                next_inputs = rand_batch[:, self.next_state_index]
-                action_args  = tf.cast(rand_batch[:, self.action_index], dtype=tf.dtypes.int32)
-                labels = tf.cast(rand_batch[:, self.reward_index], dtype=tf.dtypes.float32) + self.discount_factor * self.opt_q_val_many_states(next_inputs)
-                # get inputs and labels for neural network
-                self.update_q_weights(inputs, action_args, labels)
-                # update state
-                if np.rint(self.env.curr_state[0, 0]).astype(int) % self.reset_steps == 0:
-                    self.Q_t.set_weights(self.Q.get_weights())
-                iter_num += 1
                 state = next_state
-             
+                iter_num += 1
+            value = self.critic_model(np.array([state])).numpy()
+            values.append(value[0][0])
+            np.reshape(probs, (len(probs),self.env.num_j_temp))
+            probs = np.stack(probs, axis=0)
+            returns = []
+            adv
+            states, actions, returns, adv  = self.preprocess(states, actions, rewards, dones, values,returns, 1)
+            al,cl = self.learn(states, actions, adv, probs, returns)
             
             if episode_index % 10000 == 0:
                 self.Q.save(f"Q_{episode_index}.h5")
